7a7716ada385a6e428a36e0391749eb44b255c23
Miguel Angel Bonachea <millennial.training.mx@gmail.com>
2018-03-22 13:45:12 -0600

    new build

--- optional-kubernetes-engine/config.py
+++ optional-kubernetes-engine/config.py
@@ -18,6 +18,7 @@ Update this file with the values for your specific Google Cloud project.
 You can create and manage projects at https://console.developers.google.com
 """
 
+
 import os
 
 # The secret key is used by Flask to encrypt session cookies.
